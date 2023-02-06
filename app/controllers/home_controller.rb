d89bfa934520e8d93c93d04e4ecd71094fd29267
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2023-02-06 16:12:29 +0700

    Display select category and article

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,15 @@
 class HomeController < ApplicationController
+  helper_method :category_options, :article_options
+
   def index; end
+
+  private
+
+  def category_options
+    Category.all.pluck(:name, :id)
+  end
+
+  def article_options
+    Article.all.pluck(:title, :id)
+  end
 end
